be62f5e5cb1858552b112d34145b3000bd5ab6cc
chuross <azarasi.mo.mo@gmail.com>
2016-08-17 21:42:52 +0900

    android-sdkをroot所有にした

--- android-emulator/emulator.sh
+++ android-emulator/emulator.sh
@@ -1,18 +1,16 @@
 echo no | android create avd -n $ANDROID_EMULATOR_NAME \
-                            -t $TARGET_API \
-                            -c $STORAGE_SIZE \
-                            -s $SKIN \
+                            -t $ANDROID_TARGET_API \
+                            -c $ANDROID_EMULATOR_STORAGE_SIZE \
+                            -s $ANDROID_EMULATOR_SKIN \
                             -p $ANDROID_EMULATOR_PORT
-
-adb start-server
 
 ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT
+socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT
 
 emulator64-arm -avd $ANDROID_EMULATOR_NAME \
-                  -memory $MEMORY \
-                  -port $PORT \
+                  -memory $ANDROID_EMULATOR_MEMORY \
+                  -port $ANDROID_EMULATOR_PORT \
                   -no-boot-anim \
                   -no-window \
                   -noaudio \
