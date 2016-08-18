f27d70577ad84c579444f5dd692842bdf589220b
chuross <azarasi.mo.mo@gmail.com>
2016-08-18 11:06:07 +0900

    修正の反映

--- android-emulator/emulator.sh
+++ android-emulator/emulator.sh
@@ -1,13 +1,3 @@
-echo no | android create avd -n $ANDROID_EMULATOR_NAME \
-                            -t $ANDROID_TARGET_API \
-                            -c $ANDROID_EMULATOR_STORAGE_SIZE \
-                            -s $ANDROID_EMULATOR_SKIN \
-                            -p $ANDROID_EMULATOR_PORT
-
-ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT
-
 emulator64-arm -avd $ANDROID_EMULATOR_NAME \
                   -memory $ANDROID_EMULATOR_MEMORY \
                   -port $ANDROID_EMULATOR_PORT \
@@ -18,3 +8,10 @@ emulator64-arm -avd $ANDROID_EMULATOR_NAME \
                   -no-snapshot-save
 
 adb wait-for-device
+adb devices
+
+ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
+socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT &
+
+echo "launch emulator"
