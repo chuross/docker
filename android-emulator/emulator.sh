bfac3f6bf43ae9ce0c0d6140e5c279d843c1a267
chuross <azarasi.mo.mo@gmail.com>
2016-08-18 19:03:34 +0900

    adbで接続できるようにするところまで

--- android-emulator/emulator.sh
+++ android-emulator/emulator.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-# socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-# socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT &
-# socat tcp-listen:$ANDROID_ADB_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_ADB_PORT &
+ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT &
+socat tcp-listen:$ADB_PORT,bind=$ip,fork tcp:127.0.0.1:$ADB_PORT &
 
 emulator64-arm -avd $ANDROID_EMULATOR_NAME \
                   -port $ANDROID_EMULATOR_PORT \
