e11ef6281e82fecc7dada83ba09663ac641aba1e
chuross <azarasi.mo.mo@gmail.com>
2016-08-18 18:04:54 +0900

    エミュレータが動くところまで

--- android-emulator/emulator.sh
+++ android-emulator/emulator.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
+
+# ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+# socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
+# socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT &
+# socat tcp-listen:$ANDROID_ADB_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_ADB_PORT &
+
 emulator64-arm -avd $ANDROID_EMULATOR_NAME \
-                  -memory $ANDROID_EMULATOR_MEMORY \
                   -port $ANDROID_EMULATOR_PORT \
                   -no-boot-anim \
                   -no-window \
-                  -noaudio \
-                  -gpu off \
-                  -no-snapshot-save
+                  -no-audio \
+                  -no-snapshot-save \
+                  -gpu off &
 
 adb wait-for-device
 adb devices
-
-ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-socat tcp-listen:$ANDROID_EMULATOR_PORT,bind=$ip,fork tcp:127.0.0.1:$ANDROID_EMULATOR_PORT &
-
-echo "launch emulator"
+adb logcat
